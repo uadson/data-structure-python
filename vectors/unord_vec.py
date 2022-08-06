9f62c415e4fa4d7e451bb810b5089841fa92acbe
Uadson Feitosa <uadsonpy@gmail.com>
2022-08-06 12:00:16 -0300

    created main file as object form unord_vec.py

--- vectors/unord_vec.py
+++ vectors/unord_vec.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 class UnorderedVector:
     """
     Defining unordered vector or list
@@ -11,10 +8,9 @@ class UnorderedVector:
         values: []
     """
 
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.last_pos = -1
-        self.values = np.empty(self.capacity, dtype=int)
+    capacity: int
+    last_pos: int = -1
+    values: list = []
 
     # O(n)
     def print_values(self):
@@ -40,7 +36,8 @@ class UnorderedVector:
             self.last_pos += 1
             # last_pos(-1) + last_pos(1) = 0
             # last_pos = 0
-            self.values[self.last_pos] = value
+            # self.values[self.last_pos] = value
+            self.values.append(value)
             # from values[] to values[value]
 
     # O(n) - linear
@@ -76,47 +73,3 @@ class UnorderedVector:
                 self.values[i] = self.values[i + 1]
             print("\n#Update")
             self.last_pos -= 1
-
-
-if __name__ == "__main__":
-    # 1 print
-    print("\n# 1 - print")
-    vector = UnorderedVector(5)
-    vector.print_values()
-
-    # 2 insert
-    print("\n# 2 - insert")
-    vector.insert(1)
-    vector.print_values()
-
-    # 3 insert
-    print("\n# 3 - insert")
-    vector.insert(2)
-    vector.insert(3)
-    vector.insert(4)
-    vector.insert(5)
-    vector.print_values()
-
-    # 4 values
-    print("\n# 4 values")
-    print(vector.values)
-
-    # 5 Vector is Full
-    print("\n# 5 Vector is Full")
-    vector.insert(6)
-
-    # 6 Search value
-    print("\n# 6 Search value")
-    vector.search(4)
-
-    # 7 Search value that doesn't in the vector
-    print("\n# 7 Search value that doesn't in the vector")
-    vector.search(7)
-
-    # 8 Delete a value from vector
-    print("\n# 8 Delete a value from vector")
-    vector.print_values()
-
-    print()
-    vector.delete(3)
-    vector.print_values()
